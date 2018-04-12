64e09d905f8535774e66a0d99952f7d2aa4323a6
Danesh Madarbakus <daneshplt@hotmail.co.uk>
2018-04-12 18:01:50 +0100

    fixed loading screen

--- pokedex-react/src/components/pages/pokemon.js
+++ pokedex-react/src/components/pages/pokemon.js
@@ -4,10 +4,10 @@ class Pokemon extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          error:null,
-          isLoaded:false,
-          items: [], 
-          livePokemon: []
+            error: null,
+            isLoaded: false,
+            items: [],
+            livePokemon: []
         };
         this.loadPokemon = this.loadPokemon.bind(this);
     }
@@ -25,9 +25,9 @@ class Pokemon extends Component {
                 });
             },
             (error) => {
-          
-                    console.log('error');
-     
+
+                console.log('error');
+
             }
             )
     }
@@ -35,26 +35,27 @@ class Pokemon extends Component {
     componentDidMount() {
         console.log(this.props.match.params.topicId);
         fetch(`https://pokeapi.co/api/v2/generation/${this.props.match.params.topicId}`)
-        .then(res => res.json())
-        .then(
-            (result) => {
+            .then(res => res.json())
+            .then((result) => {
                 result.pokemon_species.reverse();
-            this.setState({
-              isLoaded:true,
-              items:result.pokemon_species
-            });
-          },
-          (error) => {
-            this.setState({
-              isLoaded: true,
-              error
-            });
-          }
-        )
+                this.setState({
+                    isLoaded: true,
+                    items: result.pokemon_species
+                });
+            },
+            (error) => {
+                this.setState({
+                    isLoaded: true,
+                    error
+                });
+            }
+            )
     }
 
-    componentWillReceiveProps (nextProps) {
-        console.log('new props');
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            isLoaded: false
+        });
         fetch(`https://pokeapi.co/api/v2/generation/${nextProps.match.params.topicId}`)
             .then(res => res.json())
             .then(
@@ -77,42 +78,42 @@ class Pokemon extends Component {
     render() {
         const { error, isLoaded, items, livePokemon } = this.state;
         const { match } = this.props;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else if (!livePokemon.sprites){
-        return (
-            <div>
-                <h1>Pokemon generation {match.params.topicId}</h1>
-                <ul>
-                    {items.map(item => (
-                        <li key={item.name} onClick={this.loadPokemon}>
-                            {item.name}
-                        </li>
-                    ))}
-                </ul>
-            </div>
-
-        )
-    } else {
-        return (
-            <div>
-                <h1>Pokemon generation {match.params.topicId}</h1>
-                <ul>
-                    {items.map(item => (
-                        <li key={item.name} onClick={this.loadPokemon}>
-                            {item.name}
-                        </li>
-                    ))}
-                </ul>
-                <div className="pokemon-holder">
-                    <img src={livePokemon.sprites.front_default} />
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else if (!livePokemon.sprites) {
+            return (
+                <div>
+                    <h1>Pokemon generation {match.params.topicId}</h1>
+                    <ul>
+                        {items.map(item => (
+                            <li key={item.name} onClick={this.loadPokemon}>
+                                {item.name}
+                            </li>
+                        ))}
+                    </ul>
                 </div>
-            </div>
 
-        )
-    }
+            )
+        } else {
+            return (
+                <div>
+                    <h1>Pokemon generation {match.params.topicId}</h1>
+                    <ul>
+                        {items.map(item => (
+                            <li key={item.name} onClick={this.loadPokemon}>
+                                {item.name}
+                            </li>
+                        ))}
+                    </ul>
+                    <div className="pokemon-holder">
+                        <img src={livePokemon.sprites.front_default} />
+                    </div>
+                </div>
+
+            )
+        }
     }
 }
 
